45f5f9b25dfcc04302920c4b918310e8aaf755f3
JosephRDoherty <109316023+JosephRDoherty@users.noreply.github.com>
2022-10-12 20:16:25 -0700

    It worked

---
+++ header.js
@@ -0,0 +1,12 @@
+
+function loadheader() {
+    const navHeader = document.getElementById("navHeader")
+
+    navHeader.innerHTML = 
+    "<div class='header'>" + 
+    "<h3>The header says this now</h3>" +
+    "<ul>" +
+    "<li><a href='./index.html'>Home</a></li>" + 
+    "<li><a href='./aboutus.html'>About Us</a></li>"+
+    "</div>";
+}
